43bc1359f77b8712d63b2432cd2d65c37576c715
Aslan Tashtanov <aslantashtanov@gmail.com>
2022-05-21 19:37:45 -0400

    cleanup, redo asteroid153814

--- contracts/Asteroid-153814.sol
+++ contracts/Asteroid-153814.sol
@@ -11,35 +11,47 @@ import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol"
 contract Asteroid153814 {
     MineEmpireDrill public drill;
     ERC20PresetMinterPauser public iron;
-    ERC20PresetMinterPauser public gold;
+    ERC20PresetMinterPauser public cosmicCash;
     address public owner;
+    address payable treasury;
     uint public baseProduction;
-    uint public capacityPerUnit;
-    uint public refinerCapacity;
-    uint[] private refinerEfficiency;
-    uint[] private refinerUpgradeRequirement;
-    uint private maxRefinerLevel;
-
     struct Stake {
         MineEmpireDrill.Drill drill;
         uint timestamp;
     }
 
+    uint maxLevel;
+    mapping(uint => uint) public capacityAtLevel;
+    mapping(uint => uint) public upgradeRequirementAtLevel;
+    mapping(address => uint) public userLevel;
     mapping(address => Stake) public stakes;
-    mapping(address => uint) public refinerLevel;
-    mapping(address => uint) public lastRefined;
 
-    constructor(ERC20PresetMinterPauser inputResource, ERC20PresetMinterPauser outputResource, MineEmpireDrill _drill) {
+    constructor(ERC20PresetMinterPauser _iron, ERC20PresetMinterPauser _cosmicCash, MineEmpireDrill _drill) {
         owner = msg.sender;
-        baseProduction = 1e16;
-        capacityPerUnit = 100;
-        refinerCapacity = 1e22;
-        iron = inputResource;
-        gold = outputResource;
+        baseProduction = 289351851900000;
+        iron = _iron;
+        cosmicCash = _cosmicCash;
         drill = _drill;
-        refinerEfficiency = [50, 55, 61, 67, 73, 80, 88, 100];
-        refinerUpgradeRequirement = [40e20, 50e20, 60e20, 70e20, 80e20, 100e20];
-        maxRefinerLevel = 7;
+        maxLevel = 9;
+        capacityAtLevel[0] = 175e18;
+        capacityAtLevel[1] = 185e18;
+        capacityAtLevel[2] = 200e18;
+        capacityAtLevel[3] = 225e18;
+        capacityAtLevel[4] = 255e18;
+        capacityAtLevel[5] = 295e18;
+        capacityAtLevel[6] = 350e18;
+        capacityAtLevel[7] = 410e18;
+        capacityAtLevel[8] = 485e18;
+        capacityAtLevel[9] = 575e18;
+        upgradeRequirementAtLevel[1] = 100e16;
+        upgradeRequirementAtLevel[2] = 120e16;
+        upgradeRequirementAtLevel[3] = 156e16;
+        upgradeRequirementAtLevel[4] = 2184e15;
+        upgradeRequirementAtLevel[5] = 3276e15;
+        upgradeRequirementAtLevel[6] = 5242e15;
+        upgradeRequirementAtLevel[7] = 8911e15;
+        upgradeRequirementAtLevel[8] = 16039e15;
+        upgradeRequirementAtLevel[9] = 30475e15;
     }
 
     function onERC721Received(
@@ -51,12 +63,11 @@ contract Asteroid153814 {
         return bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"));
     }
 
-    // events
-    event LogRefinement(uint _amountMinted);
-    event LogRefinerUpgraded(uint _newLevel);
-    // modifiers
+    // EVENTS
+
+    // MODIFIERS
     modifier onlyOwner(address _address) {
-        require(_address == owner);
+        require(_address == owner, "not authorized");
         _;
     }
 
@@ -65,127 +76,103 @@ contract Asteroid153814 {
         _;
     }
 
-    // owner only
-    function setRefinerCapacity(uint _cap) public onlyOwner(msg.sender) {
-        refinerCapacity = _cap;
+    modifier drillNotStaked(address _address) {
+        require(stakes[_address].timestamp == 0, "drill already staked");
+        _;
     }
 
-    function updateRefinerEfficiency(
-        uint[] memory newEfficiencyList
-        ) public onlyOwner(msg.sender) {
-        for (uint i = 0; i < maxRefinerLevel; i++) {
-            require(newEfficiencyList[i] != 0, "invalid input for available level");
-        }
-        refinerEfficiency = newEfficiencyList;
+    modifier levelExists(uint _level) {
+        require(_level <= maxLevel, "level greater than max level");
+        _;
     }
 
-    function updateRefinerUpgradeRequirements(
-        uint[] memory newRefinerUpgradeRequirements
-    ) public onlyOwner(msg.sender) {
-        for (uint i = 0; i < maxRefinerLevel; i++) {
-            require(newRefinerUpgradeRequirements[i] != 0, "invalid input for available level");
-        }
-        refinerUpgradeRequirement = newRefinerUpgradeRequirements;
+    // ONLY OWNER
+    
+    // update treasury
+    function updateTreasury(address payable _address) public onlyOwner(msg.sender) {
+        treasury = _address;
     }
 
-    function updateMaxRefinerLevel(uint newLevel) public onlyOwner(msg.sender) {
-        maxRefinerLevel = newLevel;
+    // update max level
+    function updateMaxLevel(uint _level) public onlyOwner(msg.sender) {
+        maxLevel = _level;
     }
 
-    // user interface
-    function stake(uint256 _drillId) public {
-        require(stakes[msg.sender].timestamp == 0, "user already staked");
-        drill.safeTransferFrom(msg.sender, address(this), _drillId);
-        stakes[msg.sender] = Stake(drill.getDrill(_drillId), block.timestamp);
+    // update capacity at level
+    function updateCapacityAtLevel(uint _level, uint _capacity) public onlyOwner(msg.sender) levelExists(_level) {
+        capacityAtLevel[_level] = _capacity;
     }
 
-    function unstake() public {
-        require(stakes[msg.sender].timestamp > 0, "drill not staked");
-        drill.safeTransferFrom(address(this), msg.sender, stakes[msg.sender].drill.drillId);
-        delete stakes[msg.sender];
+    // update upgrade requirement
+    function updateUpgradeRequirementAtLevel(uint _level, uint _capacity) public onlyOwner(msg.sender) levelExists(_level){
+        require(_level <= maxLevel, "level not less than max level");
+        upgradeRequirementAtLevel[_level] = _capacity;
     }
 
-    function collectResources() public {
-        Stake memory curStake = stakes[msg.sender];
-        uint miningMultiplier = drill.getDrillMiningPower(curStake.drill.drillId);
-        uint capacity = drill.getDrillCapacity(curStake.drill.drillId);
-        uint time = block.timestamp - curStake.timestamp;
-        uint amount = time * baseProduction / 100 * miningMultiplier / 100 * capacityPerUnit;
-        if (capacity < amount) {
-            amount = capacity;
-        }
-        iron.mint(msg.sender, amount);
-        stakes[msg.sender].timestamp = block.timestamp;
+    // GETTERS
+
+    // get capacity at level
+    function getCapacityAtLevel(uint _level) public view levelExists(_level) returns(uint) {
+        return capacityAtLevel[_level];
     }
 
-    function refine(uint _amount) public {
-        uint timeSinceLastRefine = block.timestamp - lastRefined[msg.sender];
-        if (timeSinceLastRefine > 86400) {
-            timeSinceLastRefine = 86400;
-        }
-        require(_amount <= refinerCapacity * timeSinceLastRefine / 864000, 
-                "amount to refine exceeds daily capacity");
-
-        iron.transferFrom(msg.sender, address(this), _amount);
-        uint userRefinerLevel = refinerLevel[msg.sender];
-        if (userRefinerLevel == 0) {
-            refinerLevel[msg.sender] = 1;
-            userRefinerLevel = 1;
-        }
-        uint amountToMint = _amount * refinerEfficiency[userRefinerLevel-1] / 100;
-        gold.mint(msg.sender, amountToMint);
-        lastRefined[msg.sender] = block.timestamp;
-        emit LogRefinement(amountToMint);
+    // get user level
+    function getUserLevel(address _userAddress) public view returns(uint) {
+        return userLevel[_userAddress];
     }
 
-    function upgradeRefiner() public {
-        if (refinerLevel[msg.sender] == 0) {
-            refinerLevel[msg.sender] = 1;
-        }
-        uint curLevel = refinerLevel[msg.sender];
-        require(curLevel < maxRefinerLevel, "refiner at max level");
-        uint requiredAmount = refinerUpgradeRequirement[curLevel - 1];
-        iron.transferFrom(msg.sender, address(this), requiredAmount);
-        refinerLevel[msg.sender] = curLevel + 1;
-        emit LogRefinerUpgraded(curLevel + 1);
-    }
-
-    // getters
+    // get stake
     function getStake(address _address) public view returns(Stake memory) {
         return stakes[_address];
     }
+    
+    // get base production
+    function getBaseProduction() public view returns(uint) {
+        return baseProduction;
+    }
 
-    function getAccumulatedRocks() public view returns (uint) {
-        require(stakes[msg.sender].timestamp > 0, "no drill staked");
+    // get max level
+    function getMaxLevel() public view returns(uint) {
+        return maxLevel;
+    }
+
+    // get accumulated iron
+    function getAccumulatedIron(address _userAddress) public view drillStaked(_userAddress) returns(uint) {
         Stake memory curStake = stakes[msg.sender];
         uint time = block.timestamp - curStake.timestamp;
-        uint miningMultiplier = drill.getDrillMiningPower(curStake.drill.drillId);
-        uint capacity = drill.getDrillCapacity(curStake.drill.drillId);
-        uint amount = time * baseProduction / 100 * miningMultiplier / 100 * capacityPerUnit;
+        uint miningPower = drill.getMiningPower(curStake.drill.drillId);
+        uint capacity = capacityAtLevel[userLevel[_userAddress]];
+        uint amount = time * baseProduction / 100 * miningPower / 100;
         if (capacity < amount) {
             amount = capacity;
         }
         return amount;
     }
 
-    function getBaseProduction() public view returns (uint) {
-        return baseProduction;
+    // USER INTERACTIONS
+
+    // stake
+    function stake(uint _drillId) public drillNotStaked(msg.sender) {
+        drill.safeTransferFrom(msg.sender, address(this), _drillId);
+        stakes[msg.sender] = Stake(drill.getDrill(_drillId), block.timestamp);
     }
 
-    function getCapacityPerUnit() public view returns (uint) {
-        return capacityPerUnit;
+    // collect
+    function collectIron() public drillStaked(msg.sender) {
+        uint amount = getAccumulatedIron(msg.sender);
+        iron.mint(msg.sender, amount);
+        stakes[msg.sender].timestamp = block.timestamp;
     }
 
-    function getRefinerCapacity() public view returns (uint) {
-        return refinerCapacity;
+    // unstake and collect
+    function unstake() public drillStaked(msg.sender) {
+        uint amount = getAccumulatedIron(msg.sender);
+        iron.mint(msg.sender, amount);
+        drill.safeTransferFrom(address(this), msg.sender, stakes[msg.sender].drill.drillId);
+        delete stakes[msg.sender];
     }
 
-    function getRefinerUpgradeRequirement() public view returns (uint) {
-        uint userRefinerLevel = refinerLevel[msg.sender];
-        if (userRefinerLevel == 0) {
-            userRefinerLevel = 1;
-        }
-        return refinerUpgradeRequirement[userRefinerLevel-1];
-    }
+    // upgrade
+    
 
 }
